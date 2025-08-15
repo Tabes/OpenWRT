630bdac234c6f4a905b5ce6af00fc569b03c796a
Tabes <github@mawage.de>
2025-08-15 11:47:00 +0000

    A9671:47 PM227 967p15PM1:47 PMe

--- start.cmd
+++ start.cmd
@@ -4,16 +4,19 @@
 ### Execute from USB: /mnt/usb/OpenWRT/start.cmd ###
 
 
-### USB mounten                             ###
-### mkdir -p /mnt/usb                       ###
-### mount /dev/sda1 /mnt/usb                ###
+### USB mounten                                 ###
+### mkdir -p /mnt/usb                           ###
+### mount /dev/sda1 /mnt/usb                    ###
 
-### Script ausführbar machen                ###
-### chmod +x /mnt/usb/OpenWRT/start.cmd     ###
+### Script ausführbar machen                    ###
+### chmod +x /mnt/usb/OpenWRT/start.cmd         ###
 
-### Script starten                          ###
-### cd /mnt/usb/OpenWRT                     ###
-### ./start.cmd                             ###
+### Windows-Zeilenendings entfernen
+### sed -i 's/\r$//' /mnt/usb/OpenWRT/start     ###
+
+### Script starten                              ###
+### cd /mnt/usb/OpenWRT                         ###
+### ./start.cmd                                 ###
 
 
 ### === PATH CONFIGURATION === ###
